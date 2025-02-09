480ae236eeeacaa5ef5b8edd8f1d0fe663946573
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-02-09 13:41:47 +0300

    Update GameSelector.tsx

--- components/form/GameSelector.tsx
+++ components/form/GameSelector.tsx
@@ -15,6 +15,7 @@ const GameSelector: React.FC<GameSelectorProps> = ({ onGameChange }) => (
             <SelectContent className='bg-dark-200 border-dark-300'>
                 <SelectGroup>
                     <SelectLabel className='text-white'>Which game do you prefer?</SelectLabel>
+                    <SelectItem className='text-white focus:text-white hover:bg-dark-300 focus:bg-dark-300' value="beamng">BeamNG.Drive</SelectItem>
                     <SelectItem className='text-white focus:text-white hover:bg-dark-300 focus:bg-dark-300' value="fortnite">Fortnite</SelectItem>
                     <SelectItem className='text-white focus:text-white hover:bg-dark-300 focus:bg-dark-300' value="lol">League of Legends</SelectItem>
                     <SelectItem className='text-white focus:text-white hover:bg-dark-300 focus:bg-dark-300' value="mc">Minecraft</SelectItem>
